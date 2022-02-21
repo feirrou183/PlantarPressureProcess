11bf845bfcf6b2bdf8717cee0634c0840d6616da
feirou <823246785@qq.com>
2022-02-21 15:58:33 +0800

    单样本单相位选择绘制
    单样本单相位选择绘制

---
+++ DrawPicture/DrawSingleTrail.py
@@ -0,0 +1,52 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import ProcessProgram.Velocity.PostionVelocityDataPreProcess as dicManager
+from matplotlib import cm
+import cv2 as cv
+
+
+# 设置工作环境
+Work_Path = "F:\\PlantarPressurePredictExperiment"
+os.chdir(Work_Path)
+
+
+def drawTrail(subject,detailItemName,Index,End):
+    start = 669
+    end = 681
+    plt.style.use("ggplot")
+    cmap = cm.get_cmap('jet')
+    plt.rcParams["axes.grid"] = False
+    HLArr = GetArr("subject03", "5-1", start, end)
+    plt.figure()
+
+    for i in range(end-start):
+        picture = HLArr[i].reshape(60,21)
+        plt.subplot(1,end-start,i+1)
+        plt.imshow(picture,cmap=cmap)
+        plt.axis("off")
+        plt.xticks([])
+        plt.yticks([])
+
+    plt.show()
+
+
+
+
+
+
+
+def GetArr(subject,detailItemName,HL,TO):
+
+    file = open("ProcessedData\\{0}\\{1}-R.csv".format(subject,detailItemName),encoding="utf-8")
+    Arr = np.loadtxt(file,delimiter = ",")
+
+    TOArr = Arr[HL:TO,:]
+    file.close()
+
+    return TOArr
+
+
+if __name__ == '__main__':
+    drawTrail("subject03","1-1",683,695)            #画PO的相位图
